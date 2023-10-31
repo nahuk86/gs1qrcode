20c5549ac7f711938b22314ed92dd34e1eec84c9
Nahuel Adrian Krauss <130124513+nahuk86@users.noreply.github.com>
2023-10-31 20:13:49 -0300

    cr

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function generarCodigoQR() {
     var url = document.getElementById('linkInput').value;
     if (url !== '') {
-        var qr = new QRCode(document.getElementById('qrcode'), {
-            text: url,
-            width: 128,
-            height: 128
+        JsBarcode("#barcode", url, {
+            format: "CODE128",
+            lineColor: "#000",
+            width: 2,
+            height: 40,
         });
     } else {
         alert('Por favor ingrese una URL válida.');
     }
 }
+
