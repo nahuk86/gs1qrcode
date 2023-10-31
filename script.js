2a7f4976159f7949f375ad532668c541c570780e
Nahuel Adrian Krauss <130124513+nahuk86@users.noreply.github.com>
2023-10-31 20:02:22 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function generarCodigoQR() {
+    var url = document.getElementById('linkInput').value;
+    if (url !== '') {
+        JsBarcode("#barcode", url, {
+            format: "CODE128",
+            displayValue: true,
+            lineColor: "#000",
+            width: 2,
+            height: 40,
+        });
+    } else {
+        alert('Por favor ingrese una URL válida.');
+    }
+}
